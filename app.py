411910ecf024187827baaab7e850b84f38b496b1
ArihantSrivastava2225 <89091503+ArihantSrivastava2225@users.noreply.github.com>
2024-12-19 13:34:40 +0000

    added dynamic data and added routes

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__) #created a flask application
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+  }
+]
 
 # now we need to tell flask what it should return when a certain url is requested. Hence we also need to register routes which are part of the url after the domain names
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World wow!</p>"
+    return render_template('home.html', jobs=JOBS, company_name='Jovian')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 print(__name__)
 if __name__ == "__main__":
